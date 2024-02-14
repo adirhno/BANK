111598a91a0b9cd8e96a93ce920ea10a93688b18
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-02-14 13:49:45 +0200

    last commit

---
+++ src/server/config.js
@@ -0,0 +1,20 @@
+const { default: axios } = require("axios");
+const API = 'http://localhost:3001'
+
+const calculateCategoryAmount = function (category) {
+	let sum = 0;
+	for (let i = 0; i < category.length; i++) {
+		sum += category[i].amount;
+	}
+	return sum;
+};
+
+const calculateBalance = function (allCategories) {
+	let sum = 0;
+	for (let i of Object.keys(allCategories)) {
+		sum += allCategories[i];
+	}
+	return sum;
+};
+
+module.exports = { calculateCategoryAmount, calculateBalance, API } ;
