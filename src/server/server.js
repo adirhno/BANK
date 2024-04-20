12dbee283de5ffb4e0cd85d4ba579fe1f00e6d0e
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-04-20 04:36:14 +0300

    update

--- src/server/server.js
+++ src/server/server.js
@@ -19,12 +19,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 	.catch((err) => console.log(err));
 
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: `${API}`}))
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()
 })
+app.use(cors({credentials: true, origin: `${API}`}))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(api);
