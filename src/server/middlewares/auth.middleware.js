eccb2233e68c4453e5b1c530f7bf2a710d945801
adir hino <adirhno@adirs-MacBook-Pro.local>
2024-04-21 03:32:42 +0300

    a

--- src/server/middlewares/auth.middleware.js
+++ src/server/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ function authorizationMiddleWare(req, res, next) {
 	const { user } = req.cookies;
 
 	try {
-		if (token !== undefined) {		
+		if (token !== undefined) {
 			console.log("im here");
 			const payload = jwt.verify(token, "token", (e) => {});
 			if (payload) {
@@ -26,12 +26,22 @@ function authorizationMiddleWare(req, res, next) {
 					res.clearCookie("token").send("Invalid JWT token!");
 				}
 			}
-		} else{
-			res.send("not yet")
+		} else {
+			const refreshToken = jwt.verify(refresh, "refresh");
+			if (refreshToken) {
+				const newToken = jwt.sign({ user: user }, "token", {
+					expiresIn: "100s",
+				});
+				res.cookie("token", newToken, {
+					httpOnly: true,
+					maxAge: 900000,
+				});
+				next();
+			} else {
+				res.clearCookie("token").send("Invalid JWT token!");
+			}
 		}
-		
 	} catch (error) {
-		console.log(error);
 		res.clearCookie("token").send("Invalid JWT token!");
 	}
 }
